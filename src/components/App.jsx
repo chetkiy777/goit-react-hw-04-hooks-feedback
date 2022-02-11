8458244452358ec53822b6d1a2bf14070996b2c8
Igor Gladkiy <egunka1988@gmail.com>
2022-02-11 14:08:36 +0200

    configure and add html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
+let state = { good: 0, neutral: 0, bad: 0 };
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Please Leave Feadback</h1>
+
+      <div>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+
+      <section>
+        <h1>Statistic</h1>
+        <label>
+          Good <span>{state.good}</span>
+        </label>
+
+        <label>
+          Neutral <span>{state.neutral}</span>
+        </label>
+
+        <label>
+          Bad <span>{state.bad}</span>
+        </label>
+      </section>
     </div>
   );
 };
